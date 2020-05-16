89426ee3347c32a3c9bdedf26e9efaa88e52369d
yourcoachk <42878676+yourcoachk@users.noreply.github.com>
2020-05-16 09:31:40 -0400

    Update Comparator1.py

--- Comparator1.py
+++ Comparator1.py
@@ -5,10 +5,7 @@ Created on Fri May 15 11:21:37 2020
 @author: Kolby Kuratnick
 """
 
-import json
-
-
-zipcode = 19701
+zipcode = 19701                             #This is just a test example
 prop_type_str = 'Single Family Home'
 year_built = 1997
 #prop_value = 133*2000
@@ -28,13 +25,13 @@ microwave = 'yes'
 stove = 'yes'
 #kitchen_match = 'yes'
 #washer_dryer_match = 'yes'
-pool = 'no'
+pool = 'none'
 hot_tub = 'no'
 driveway = 'yes'
 garage = 'yes'
 #garage_install = 
 AC_type_str = 'Central Air'
-heat_type_str = 'Standard Heat Pump'
+heat_type_str = 'Heat Pump'
 fireplaces = 1
 electric_system = 'circuit breaker'
 water_type = 'town'
@@ -44,6 +41,18 @@ patio = 'yes'
 yard = 'yes'
 totalValue = 329900
 
+
+
+import json
+import numpy as np
+
+
+
+def getList(dict):
+    list = []
+    for key in dict.keys():
+        list.append(key)
+    return list
 
 
 
@@ -89,14 +98,12 @@ data['Results'].append({
     "Total Value" : totalValue
 
 })
-
-
-
+    
+    
+    
 with open("results.json", "w") as outfile:
     json.dump(data, outfile)
     input_results = data
-
-
 
 with open("C://Users/Kolby Kuratnick/Documents/api_results.json") as json_file:
     api_results = json.load(json_file)
@@ -115,7 +122,7 @@ for i in range (1,200):
 common_prop_sqft = dict()
 input_prop_sqft = input_results['Results'][0]['prop_sqft']
 for i in range (1,200):
-    if  (input_prop_sqft-100) <= api_results[str(i)]['prop_sqft'] and (input_prop_sqft+100) >= api_results[str(i)]['prop_sqft']:
+    if  (input_prop_sqft-150) <= api_results[str(i)]['prop_sqft'] and (input_prop_sqft+150) >= api_results[str(i)]['prop_sqft']:
         common_prop_sqft[str(i)] = api_results[str(i)]['prop_sqft']
 common_bedrooms = dict()
 for i in range (1,200):
@@ -151,14 +158,45 @@ for i in range (1,200):
         common_basement[str(i)] = api_results[str(i)]['basement']
 
 
-best_comps = dict()
-c = 0
-#for str(a) in range (0,len(common_bedrooms)):
-#    if (common_bedrooms[str(a)] and common_full_bathrooms[str(a)]):
-#        best_comps[str(c)] = common_bedrooms[str(a)]
-#        c = c+1
-#    else:
-#        pass
-#    
+common_year_list = getList(common_year)
+common_prop_type_list = getList(common_prop_type)
+common_prop_sqft_list = getList(common_prop_sqft)
+common_bedrooms_list = getList(common_bedrooms)
+common_full_bathrooms_list = getList(common_full_bathrooms)
+common_half_bathrooms_list = getList(common_half_bathrooms)
+common_roof_type_list = getList(common_roof_type)
+common_pool_list = getList(common_pool)
+common_AC_type_list = getList(common_AC_type)
+common_heat_type_list = getList(common_heat_type)
+common_basement_list = getList(common_basement)
 
 
+
+best_comp_iter1 = np.intersect1d(common_bedrooms_list, common_full_bathrooms_list)
+if (len(best_comp_iter1) > 5):
+    best_comp_iter2 = np.intersect1d(best_comp_iter1, common_prop_sqft_list)
+else:
+    best_comp_iter2 = best_comp_iter1
+if (len(best_comp_iter2) > 5):
+    best_comp_iter3 = np.intersect1d(best_comp_iter2, common_year_list)
+else:
+    best_comp_iter3 = best_comp_iter2
+if (len(best_comp_iter3) > 5):
+    best_comp_iter4 = np.intersect1d(best_comp_iter3, common_half_bathrooms_list)
+else:
+    best_comp_iter4 = best_comp_iter3
+if (len(best_comp_iter4) > 5):
+    best_comp_iter5 = np.intersect1d(best_comp_iter4, common_prop_type_list)
+else:
+    best_comp_iter5 = best_comp_iter4
+
+
+
+best_comp_array = np.array([best_comp_iter5])
+best_comp_list = best_comp_array.tolist()
+
+best_comps_info = [1]*len(best_comp_list[0])
+for a in range (0, len(best_comps_info)):
+    for b in range (0, 199):
+        if (str(best_comp_list[0][a]) in api_results):
+            best_comps_info[a] = api_results[str(best_comp_list[0][a])]
